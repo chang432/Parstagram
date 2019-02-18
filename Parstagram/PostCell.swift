d4204b936b17c98aec43b54c0745324546117e32
andre <chang432@purdue.edu>
2019-02-17 19:36:25 -0500

    stuff

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Andre Chang on 2/17/19.
+//  Copyright © 2019 Andre Chang. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
